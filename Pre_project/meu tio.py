21c5575520ae424a1941f56cf86cf609792c1859
Abhay <4bh4y.m3nd3s@gmail.com>
2023-12-07 17:16:56 +0000

    Update

--- Pre_project/meu tio.py
+++ Pre_project/meu tio.py
@@ -8,10 +8,33 @@
 # except ValueError:
 #     print("Por favor, digite um número inteiro válido.")
 
-def exemplo_indetacao(valor):
-    if valor > 10:
-        print("Valor é maior que 10")
-    else:
-        print("O Valor é igual ou menor que 10")
+# def exemplo_indetacao(valor):
+#     if valor > 10:
+#         print("Valor é maior que 10")
+#     else:
+#         print("O Valor é igual ou menor que 10")
 
-exemplo_indetacao(15)
+# exemplo_indetacao(15)
+
+# Exemplo de valores Falsy em Python
+falsy_values = [False, None, 0, 0.0, '', [], {}]
+
+# Exemplo de valores Truthy em Python
+truthy_values = [True, 1, 3.14, 'texto', [1, 2, 3], {'chave': 'valor'}]
+
+# Função para verificar se um valor é Truthy ou Falsy
+def verificar_truthy_falsy(valor):
+    if valor:
+        print(f'O valor {repr(valor)} é Truthy.')
+    else:
+        print(f'O valor {repr(valor)} é Falsy.')
+
+# Testando os valores Falsy
+print("Valores Falsy:")
+for valor in falsy_values:
+    verificar_truthy_falsy(valor)
+
+# Testando os valores Truthy
+print("\nValores Truthy:")
+for valor in truthy_values:
+    verificar_truthy_falsy(valor)
